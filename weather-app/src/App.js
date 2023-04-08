daf75201681a152fbfccb9575dbd38a48a2fcd0c
LarcomLabs <damonlarcom22@gmail.com>
2023-04-08 02:00:27 -0600

    added icons, added custom font

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,5 +1,6 @@
 import { useRef, useState} from "react";
 import WeatherContainer from "./components/WeatherContainer";
+import {BsCloudRainFill} from 'react-icons/bs'
 
 function App() {
   const [city, setCity] = useState('')
@@ -8,15 +9,20 @@ function App() {
   return (
     <div className="App">
       <div className="flex flex-col justify-center items-center min-h-screen bg-slate-800">
-        <h1 className="text-5xl text-white">5-day Forecast</h1>
+        <div className="flex text-5xl text-white">
+          <BsCloudRainFill/>
+          <h1 className="font-chivo mx-5">5-day Forecast</h1>
+          <BsCloudRainFill/>
+        </div>
+        
         <div id="city" className="flex">
-           <input type="text" className="w-56 h-16 text-2xl rounded-md text-center my-5 mx-2" placeholder="Enter a city.." ref={cityRef}/>
-          <button className="w-36 h-16 text-white rounded-lg shadow-xl bg-slate-400 my-5 mx-2" onClick={() => setCity(cityRef.current.value)}>Update City</button>
+           <input type="text" className="w-80 h-16 text-2xl rounded-md text-center my-5 mx-2 font-chivo" placeholder="Enter a city.." ref={cityRef}/>
+          <button className="w-36 h-16 text-lg text-white font-chivo rounded-lg shadow-xl bg-slate-400 my-5 mx-2" onClick={() => setCity(cityRef.current.value)}>Update City</button>
         </div>
         {city? 
           <WeatherContainer search={city}/>
           :
-          null
+          <p className="text-3xl text-white">No City Selected..</p>
         }
       </div> 
     </div>
