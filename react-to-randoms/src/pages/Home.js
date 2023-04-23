cb9ebe48a79de5da7b5a3329bea72251d69d1dab
Damon Larcom <dlarcom@larcomlabs.com>
2023-04-23 13:55:49 -0600

    update for other pc

--- react-to-randoms/src/pages/Home.js
+++ react-to-randoms/src/pages/Home.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import {React, useState, useEffect} from 'react'
 
 const Home = () => {
+    const [users, setUsers] = useState([])
+
+    const url = "https://randomuser.me/api/?results=500"
+
+    useEffect(() =>{
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setUsers(data.results))
+        console.log(users)
+    }, [])
+
   return (
-    <div>Home</div>
+    <div>
+        <div>Home</div>
+        {users.map((user, index) => {
+            return (
+                <div className='border-2 rounded-md shadow-lg'>{user.name.first}, {user.name.last}</div>
+            )
+        })}
+    </div>
+    
   )
 }
 
