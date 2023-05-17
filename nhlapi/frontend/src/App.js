5e30457e51b78f7eaddba5056634e7659ade26ff
Damon Larcom <dlarcom@larcomlabs.com>
2023-05-17 11:50:31 -0600

    routing

--- nhlapi/frontend/src/App.js
+++ nhlapi/frontend/src/App.js
@@ -1,8 +1,18 @@
+import {BrowserRouter as Router, Routes,Route, Link} from 'react-router-dom'
+import NotFound from "./components/NotFound"
+import LoginPage from './components/LoginPage';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App min-h-screen bg-slate-800">
+      <Router>
+        <Routes>
+          <Route exact path="/login" element={<LoginPage/>}/>
 
+          {/* 404 page matches all unspecified paths */}
+          <Route path="*" element={<NotFound/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
